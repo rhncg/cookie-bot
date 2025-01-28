73f32871bb78d34f7fb31966bcdf8b9f06fc4e05
RHNCG21 <rohanchakgupta@gmail.com>
2025-01-28 08:14:21 -0800

    v2.2.4 (added profile user command)

--- src/cogs/profile.py
+++ src/cogs/profile.py
@@ -3,6 +3,7 @@ from numerize.numerize import numerize
 from datetime import datetime
 from src.funcs.data import get_data
 from src.funcs.level import get_xp_bar_data, calculate_level
+from src.funcs.profile import get_profile
 
 class Profile(discord.Cog):
     def __init__(self, bot):
@@ -12,28 +13,11 @@ class Profile(discord.Cog):
     async def profile(self, ctx, user: discord.User = None):
         if user is None:
             user = ctx.author
-        try:
-            data = await get_data(user.id)
-            balance = data['balance']
-            bake_speed = data['bake_speed']
-            oven_cap = data['oven_cap']
-            idle_upgrade_level = data['idle_upgrade_level']
-            idle_upgrade = round(1.15 ** (idle_upgrade_level - 1) - 1, 1)
-            bar_data = await get_xp_bar_data(data['xp'])
-            embed = discord.Embed(color=0x6b4f37)
-            embed.add_field(name=f"Level {await calculate_level(data['xp'])} - {numerize(data['xp'], 2)} xp", value=bar_data[3],
-                            inline=False)
-            embed.add_field(name=f"{numerize(bar_data[1] - data['xp'], 2)} xp to level {await calculate_level(data['xp']) + 1}",
-                            value="", inline=False)
-            embed.add_field(name="Balance", value=f"{numerize(balance, 2)} cookies", inline=True)
-            embed.add_field(name="Bake Speed", value=f"{numerize(bake_speed, 2)} seconds", inline=True)
-            embed.add_field(name="Oven Capacity", value=f"{numerize(oven_cap, 2)} cookies", inline=True)
-            embed.add_field(name="Idle Rate", value=f"{numerize(idle_upgrade, 2)} cookies per minute", inline=True)
-            embed.add_field(name="Total Cookies Baked", value=numerize(data['total_cookies'], 2), inline=True)
-            embed.set_author(name=f"{user.name}'s profile", icon_url=user.display_avatar.url)
-            await ctx.respond(embed=embed)
-        except Exception as e:
-            await ctx.respond(f"That isn't a valid user. {e}", ephemeral=True)
+        await get_profile(ctx, user)
+    
+    @discord.user_command(name="Profile")
+    async def user_profile(self, ctx, user: discord.Member):
+        await get_profile(ctx, user)
             
     @discord.command()
     async def cooldowns(self, ctx):
--- src/cogs/updates.py
+++ src/cogs/updates.py
@@ -8,13 +8,14 @@ class Updates(discord.Cog):
     @discord.command()
     async def updates(self, ctx):
         embed = discord.Embed(title="Updates", color=0x6b4f37)
-        embed.add_field(name="Version", value="2.2.3", inline=False)
+        embed.add_field(name="Version", value="2.2.4", inline=False)
         embed.add_field(name="Completed (in order of completion)", value="- Buffed Idle Upgrade (higher rate now)\n"
                                                 "- Fixed stealing bug\n"
                                                 "- Boosts are now available\n"
                                                 "- Added options menu\n"
                                                 "- Boosts are cheaper to activate now\n"
-                                                "- Drops", inline=False)
+                                                "- Drops\n"
+                                                "- You can now right click a user and go to \"Apps\" to steal from them or view their profile.", inline=False)
         embed.add_field(name="Upcoming (in no particular order)", value="- Leaderboard Improvements\n"
                                                 "- Boost Duration Upgrades\n"
                                                 "- Better Gambling\n"
