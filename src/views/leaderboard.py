409f6441bd0757c0e838b973dc311d7401ce795b
RHNCG21 <rohanchakgupta@gmail.com>
2025-02-14 14:20:38 -0800

    v2.5.2

--- src/views/leaderboard.py
+++ src/views/leaderboard.py
@@ -36,12 +36,48 @@ class LeaderboardView(discord.ui.View):
 
         await interaction.response.edit_message(embed=embed, view=self)
         
-    @discord.ui.button(label="", emoji="➡️", style=discord.ButtonStyle.secondary)
-    async def next_callback(self, button, interaction):
-        self.page += 1
-        await interaction.response.edit_message(view=self)
-    
     @discord.ui.button(label="", emoji="⬅️", style=discord.ButtonStyle.secondary)
     async def prev_callback(self, button, interaction):
         self.page -= 1
-        await interaction.response.edit_message(view=self)
+        
+        conn = await get_db_connection()
+        cursor = await conn.cursor()
+        sort = "xp" if self.sort_by_level else "balance"
+        await cursor.execute(f"SELECT user_id, {sort} FROM users ORDER BY {sort} DESC LIMIT 10 OFFSET {(self.page) * 10}")
+        rows = await cursor.fetchall()
+
+        embed = discord.Embed(title="Leaderboard", color=0x6b4f37)
+        for i, row in enumerate(rows):
+            if row[1] == 0:
+                continue
+            if self.sort_by_level:
+                label = f"Level {await calculate_level(row[1])} - {numerize(row[1], 2)} xp"
+                embed.add_field(name="", value=f"{i + 1}. <@{row[0]}> - {label}", inline=False)
+            else:
+                label = f"{numerize(row[1], 2)} cookies"
+                embed.add_field(name="", value=f"{i + 1}. <@{row[0]}> - {label}", inline=False)
+
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @discord.ui.button(label="", emoji="➡️", style=discord.ButtonStyle.secondary)
+    async def next_callback(self, button, interaction):
+        self.page += 1
+        
+        conn = await get_db_connection()
+        cursor = await conn.cursor()
+        sort = "xp" if self.sort_by_level else "balance"
+        await cursor.execute(f"SELECT user_id, {sort} FROM users ORDER BY {sort} DESC LIMIT 10 OFFSET {(self.page) * 10}")
+        rows = await cursor.fetchall()
+
+        embed = discord.Embed(title="Leaderboard", color=0x6b4f37)
+        for i, row in enumerate(rows):
+            if row[1] == 0:
+                continue
+            if self.sort_by_level:
+                label = f"Level {await calculate_level(row[1])} - {numerize(row[1], 2)} xp"
+                embed.add_field(name="", value=f"{i + 1}. <@{row[0]}> - {label}", inline=False)
+            else:
+                label = f"{numerize(row[1], 2)} cookies"
+                embed.add_field(name="", value=f"{i + 1}. <@{row[0]}> - {label}", inline=False)
+
+        await interaction.response.edit_message(embed=embed, view=self)
