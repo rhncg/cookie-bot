d5a6242f80234bb1fa056458d7b88c63053d4454
RHNCG21 <rohanchakgupta@gmail.com>
2025-02-14 15:22:18 -0800

    v2.5.2

--- src/views/leaderboard.py
+++ src/views/leaderboard.py
@@ -100,3 +100,35 @@ class LeaderboardView(discord.ui.View):
                 embed.add_field(name="", value=f"{i + 1 + (self.page - 1) * 10}. <@{row[0]}> - {label}", inline=False)
 
         await interaction.response.edit_message(embed=embed, view=self)
+        
+    @discord.ui.button(label="", emoji="👤", style=discord.ButtonStyle.secondary)
+    async def jumpt_to_profile(self, button, interaction):
+        conn = await get_db_connection()
+        cursor = await conn.cursor()
+        sort = "xp" if self.sort_by_level else "balance"
+        user_id = interaction.user.id
+        await cursor.execute(f"SELECT COUNT(*) + 1 AS position FROM users 
+                            WHERE {sort} > (SELECT {sort} FROM users WHERE user_id = {user_id})")
+        row = await cursor.fetchone()
+        position = row[0] if row else None
+        if position:
+            self.page = (position - 1) // 10 + 1
+            
+        conn = await get_db_connection()
+        cursor = await conn.cursor()
+        sort = "xp" if self.sort_by_level else "balance"
+        await cursor.execute(f"SELECT user_id, {sort} FROM users ORDER BY {sort} DESC LIMIT 10 OFFSET {(self.page - 1) * 10}")
+        rows = await cursor.fetchall()
+
+        embed = discord.Embed(title="Leaderboard", color=0x6b4f37)
+        for i, row in enumerate(rows):
+            if row[1] == 0:
+                continue
+            if self.sort_by_level:
+                label = f"Level {await calculate_level(row[1])} - {numerize(row[1], 2)} xp"
+                embed.add_field(name="", value=f"{i + 1 + (self.page - 1) * 10}. <@{row[0]}> - {label}", inline=False)
+            else:
+                label = f"{numerize(row[1], 2)} cookies"
+                embed.add_field(name="", value=f"{i + 1 + (self.page - 1) * 10}. <@{row[0]}> - {label}", inline=False)
+
+        await interaction.response.edit_message(embed=embed, view=self)
