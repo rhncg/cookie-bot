9ecb020170780b03cff379cd634e3c6defdd2b96
rhncg <rohancg@outlook.com>
2025-03-21 20:13:26 -0700

    v2.7.3 (db fix)

--- src/funcs/db.py
+++ src/funcs/db.py
@@ -3,5 +3,5 @@ from src.bot_instance import bot
 
 async def get_db_connection():
     if not hasattr(bot, 'db_conn'):
-        bot.db_conn = await aiosqlite.connect("data.db")
+        bot.db_conn = await aiosqlite.connect("src/data.db")
     return bot.db_conn
