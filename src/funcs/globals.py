257a201efd52e42ca6daf69752c815980e490494
rhncg <rohancg@outlook.com>
2025-03-28 15:23:25 -0700

    v2.8.4 (added reload button to leaderboard)

--- src/funcs/globals.py
+++ src/funcs/globals.py
@@ -1,6 +1,6 @@
 global admins, baking_users, bake_speed_upgrades, gamble_users
 
-version = '2.8.3'
+version = '2.8.4'
 admins = [1066616669843243048, 1099138268601258024]
 dev_bots = [1326398030710313012]
 active_channels = {}
--- src/views/leaderboard.py
+++ src/views/leaderboard.py
@@ -136,7 +136,7 @@ class LeaderboardView(discord.ui.View):
         await interaction.response.edit_message(embed=embed, view=self)
         
     @discord.ui.button(label="", emoji="👤", style=discord.ButtonStyle.secondary)
-    async def jumpt_to_profile(self, button, interaction):
+    async def jump_to_profile(self, button, interaction):
         conn = await get_db_connection()
         cursor = await conn.cursor()
         user_id = interaction.user.id
@@ -160,6 +160,38 @@ class LeaderboardView(discord.ui.View):
         await cursor.execute(f"SELECT user_id, {sort} FROM users ORDER BY {sort} DESC LIMIT 10 OFFSET {(self.page - 1) * 10}")
         rows = await cursor.fetchall()
 
+        embed = discord.Embed(title=f"{self.sort} Leaderboard", color=0x6b4f37)
+        for i, row in enumerate(rows):
+            if row[1] == 0:
+                continue
+            
+            if self.sort == "Level":
+                label = f"Level {await calculate_level(row[1])} - {numerize(row[1], 2)} xp"
+                embed.add_field(name="", value=f"{i + 1 + (self.page - 1) * 10}. <@{row[0]}> - {label}", inline=False)
+            elif self.sort == "Cookies":
+                label = f"{numerize(row[1], 2)} cookies"
+                embed.add_field(name="", value=f"{i + 1 + (self.page - 1) * 10}. <@{row[0]}> - {label}", inline=False)
+            elif self.sort == "Streak":
+                label = f"{numerize(row[1], 2)} days"
+                embed.add_field(name="", value=f"{i + 1 + (self.page - 1) * 10}. <@{row[0]}> - {label}", inline=False)
+
+        await interaction.response.edit_message(embed=embed, view=self)
+    
+    @discord.ui.button(label="", emoji="🔄", style=discord.ButtonStyle.secondary)
+    async def refresh_callback(self, button, interaction):
+        if self.sort == "Level":
+            sort = "xp"
+        elif self.sort == "Cookies":
+            sort = "balance"
+        elif self.sort == "Streak":
+            sort = "daily_streak"
+        
+        conn = await get_db_connection()
+        cursor = await conn.cursor()
+        
+        await cursor.execute(f"SELECT user_id, {sort} FROM users ORDER BY {sort} DESC LIMIT 10 OFFSET {(self.page - 1) * 10}")
+        rows = await cursor.fetchall()
+        
         embed = discord.Embed(title=f"{self.sort} Leaderboard", color=0x6b4f37)
         for i, row in enumerate(rows):
             if row[1] == 0:
