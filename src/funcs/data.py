5b6e4c020f4ac3e513d672ab3350ef7831093a2e
rhncg <rohancg@outlook.com>
2025-04-03 14:27:22 -0700

    v2.9.1 (debugging on prod again)

--- src/funcs/data.py
+++ src/funcs/data.py
@@ -82,6 +82,6 @@ async def get_user_ids():
     cursor = await conn.cursor()
     await cursor.execute("SELECT user_id FROM users")
     rows = await cursor.fetchall()
-    print(rows)
     user_ids = [row[0] for row in rows]
+    print(user_ids)
     return user_ids
