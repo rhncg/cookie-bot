0453b1a1c79d79c0d797f6a0f93addb9255afb62
rhncg <rohanchakgupta@gmail.com>
2025-04-10 13:00:10 -0700

    made achivements json

---
+++ src/funcs/achievements.py
@@ -0,0 +1,166 @@
+import discord
+
+{
+    "bake_1": {
+        "name": "Bake 10 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_50": {
+        "name": "Bake 50 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_100": {
+        "name": "Bake 100 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_500": {
+        "name": "Bake 500 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_1000": {
+        "name": "Bake 1000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_2500": {
+        "name": "Bake 2500 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_5000": {
+        "name": "Bake 5000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_10000": {
+        "name": "Bake 10000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_50000": {
+        "name": "Bake 50000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_100000": {
+        "name": "Bake 100000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_500000": {
+        "name": "Bake 500000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_1m": {
+        "name": "Bake 1000000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_5m": {
+        "name": "Bake 5000000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_10m": {
+        "name": "Bake 10000000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_50m": {
+        "name": "Bake 50000000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_100m": {
+        "name": "Bake 100000000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_500m": {
+        "name": "Bake 500000000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_1b": {
+        "name": "Bake 1000000000 cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_100b": {
+        "name": "Bake 100 billion cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_1t": {
+        "name": "Bake 1 trillion cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_100t": {
+        "name": "Bake 100 trillion cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_1qu": {
+        "name": "Bake 1 quadrillion cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_100qu": {
+        "name": "Bake 100 quadrillion cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_1qi": {
+        "name": "Bake 1 quintillion cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_100qi": {
+        "name": "Bake 100 quintillion cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "bake_1sx": {
+        "name": "Bake 1 sextillion cookies",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+    "level_1": {
+        "name": "Level 1",
+        "description": "",
+        "unlocked": False,
+        "time": 0
+    },
+}
