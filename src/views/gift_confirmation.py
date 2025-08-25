2424c9040c0058927ff7ad9fa79c723c557d406a
rhncg <rohanchakgupta@gmail.com>
2025-08-24 22:51:37 -0700

    added numerize to announce message for gifting

--- src/views/gift_confirmation.py
+++ src/views/gift_confirmation.py
@@ -1,6 +1,7 @@
 import discord
 from datetime import datetime
 from funcs.data import get_data, update_data, update_balance
+from funcs.numerize import numerize
 
 class GiftConfirmationView(discord.ui.View):
     def __init__(self, giver: discord.User, receiver: discord.User, amount: int):
@@ -16,10 +17,10 @@ class GiftConfirmationView(discord.ui.View):
         await update_data(await update_balance(giver_data, -self.amount))
         await update_data(await update_balance(await get_data(self.receiver.id), self.amount))
 
-        success_embed = discord.Embed(title=f"Gifted {self.receiver.display_name} {self.amount} cookies")
+        success_embed = discord.Embed(title=f"Gifted {self.receiver.display_name} {numerize(self.amount)} cookies")
         await interaction.response.edit_message(embed=success_embed, view=None)
-        await interaction.channel.send(content=f"{self.giver.mention} gifted {self.receiver.mention} {self.amount} cookies!")
-        
+        await interaction.channel.send(content=f"{self.giver.mention} gifted {self.receiver.mention} {numerize(self.amount)} cookies!")
+
     @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
     async def cancel_button(self, button: discord.ui.Button, interaction: discord.Interaction):
         cancel_embed = discord.Embed(title="Gift Cancelled")
